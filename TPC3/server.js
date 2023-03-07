6784b06142dac67d9177f065859a8a280ffcd92a
JoaoNovais181 <a96626@alunos.uminho.pt>
2023-03-07 23:36:24 +0000

    tpc3

--- TPC3/server.js
+++ TPC3/server.js
@@ -25,10 +25,26 @@ http.createServer( (req, res) => {
 				var pessoas = resp.data
 
 				res.writeHead(200,{'Content-Type':'text/html ; charset="utf-8"'})
-				res.end(my_pages.genListPage(pessoas, false))
+				res.end(my_pages.genListPage(pessoas, "Lista de Pessoas"))
 			})
 			.catch( (error) => {
-				res.writeHead(404, {'Contet-Type': 'text/html; charset=utf-8'})
+				res.writeHead(404, {'Contet-Type': 'text/html; charset="utf-8"'})
+				res.end("<p>ERRO: Não foi possível aceder à Base de Dados: " + error + "</p>")
+
+			})
+	}
+	else if (pedido.match(/\/pessoas\/p\d+/))
+	{
+		let id = pedido.substring(9)
+		axios.get('http://localhost:3000/pessoas/' + id)
+			.then( (resp) => {
+				var pessoa = resp.data
+
+				res.writeHead(200,{'Content-Type':'text/html ; charset="utf-8"'})
+				res.end(my_pages.genPessoaPage(pessoa))
+			})
+			.catch( (error) => {
+				res.writeHead(404, {'Contet-Type': 'text/html; charset="utf-8"'})
 				res.end("<p>ERRO: Não foi possível aceder à Base de Dados</p>")
 			})
 	}
@@ -38,15 +54,17 @@ http.createServer( (req, res) => {
 			.then( (resp) => {
 				var pessoas = resp.data
 
+				pessoas.sort( (pessoa1, pessoa2) => (pessoa1.nome < pessoa2.nome) ?-1 :1)
+
 				res.writeHead(200,{'Content-Type':'text/html ; charset="utf-8"'})
-				res.end(my_pages.genListPage(pessoas, true))
+				res.end(my_pages.genListPage(pessoas, "Lista de Pessoas Ordenada"))
 			})
 			.catch( (error) => {
 				res.writeHead(404, {'Contet-Type': 'text/html; charset="utf-8"'})
 				res.end("<p>ERRO: Não foi possível aceder à Base de Dados</p>")
 			})
 	}
-	else if (pedido == "/pessoas/distSexo")
+	else if (pedido == "/pessoas/sexo")
 	{
 		axios.get('http://localhost:3000/pessoas/')
 			.then( (resp) => {
@@ -56,18 +74,66 @@ http.createServer( (req, res) => {
 				res.end(my_pages.genListByGender(pessoas))
 			})
 			.catch( (error) => {
-				res.writeHead(404, {'Contet-Type': 'text/html; charset=utf-8'})
+				res.writeHead(404, {'Contet-Type': 'text/html; charset="utf-8"'})
 				res.end("<p>ERRO: Não foi possível aceder à Base de Dados</p>")
 			})
 	}
-	else if (pedido == "/pessoas/distDesporto")
+	else if (pedido == "/pessoas/desporto")
 	{
 		axios.get('http://localhost:3000/pessoas')
 			.then( (resp) => {
 				var pessoas = resp.data
 
 				res.writeHead(200, {'Content-Type':'text/html ; charset="utf-8"'})
-				res.end(my_pages.genSportsPage(pessoas))
+				res.end(my_pages.genSportsPage(pessoas, "Distribuição por Desporto"))
+			})
+	}
+	else if (pedido.match(/pessoas\/desporto\/\w+/))
+	{
+		axios.get('http://localhost:3000/pessoas')
+			.then( (resp) => {
+				var pessoas = resp.data
+				let desporto = decodeURI(pedido.substring(18))
+
+				pessoasFiltrado = pessoas.filter( (p) => p.desportos.includes(desporto))
+
+				res.writeHead(200, {'Content-Type':'text/html; charset="utf-8"'})
+				res.end(my_pages.genListPage(pessoasFiltrado, "Lista de Pessoas que Praticam " + desporto))
+			})
+			.catch( (error) => {
+				res.writeHead(404, {'Contet-Type': 'text/html; charset="utf-8"'})
+				res.end("<p>ERRO: Não foi possível aceder à Base de Dados</p>")
+			})
+	}
+	else if (pedido == "/pessoas/profissoes")
+	{
+		axios.get('http://localhost:3000/pessoas')
+			.then((resp) => {
+				var pessoas = resp.data
+				
+				res.writeHead(200, {'Content-Type':'text/html; charset="utf-8"'})
+				res.end(my_pages.genWorkPage(pessoas))
+			})
+			.catch( (error) => {
+				res.writeHead(404, {'Contet-Type': 'text/html; charset="utf-8"'})
+				res.end("<p>ERRO: Não foi possível aceder à Base de Dados</p>")
+			})
+	}
+	else if (pedido.match(/pessoas\/profissoes\/\w+/))
+	{
+		axios.get('http://localhost:3000/pessoas')
+			.then( (resp) => {
+				var pessoas = resp.data
+				let profissao = decodeURI(pedido.substring(20))
+
+				var pessoasFiltrado = pessoas.filter( (p) => p.profissao==profissao)
+
+				res.writeHead(200, {'Content-Type':'text/html; charset="utf-8"'})
+				res.end(my_pages.genListPage(pessoasFiltrado, "Lista de Pessoas que trabalham como: " + profissao))
+			})
+			.catch( (error) => {
+				res.writeHead(404, {'Contet-Type': 'text/html; charset="utf-8"'})
+				res.end("<p>ERRO: Não foi possível aceder à Base de Dados</p>")
 			})
 	}
 	else if (pedido.match(/\/pessoas\?(\w+=\w+)+/))
@@ -80,11 +146,10 @@ http.createServer( (req, res) => {
 				var pessoas = resp.data
 				
 				res.writeHead(200, {'Content-Type':'text/html ; charset="utf-8"'})
-				res.end(my_pages.genListPage(pessoas))
+				res.end(my_pages.genListPage(pessoas, "Lista de Pessoas"))
 			})
 			.catch( (error) => {
 				res.writeHead(200, {'Content-Type':'text/html ; charset="utf-8"'})
-				res.end(my_pages.genListPage(pessoas))
 				res.end("<p>ERRO: Não foi possível aceder à Base de Dados</p>")
 			})
 	}
